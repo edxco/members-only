d1183b891e3a4c324dfc015cf0973f5e3b6bcaa0
edxco <eduardo.baeza@yahoo.com>
2020-11-24 14:10:07 -0600

    Post Model validations update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   #belongs_to :user
   validates :title, length: { minimum: 5 }, presence: true
-  validates :description, length: { minimum: 5 }, presence: true
+  validates :content, length: { minimum: 5 }, presence: true
 end
